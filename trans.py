157973eaba559dd56d345f7509bf288d54f59a48
Khushhal Reddy <58569950+KKhushhalR2405@users.noreply.github.com>
2020-01-07 20:31:20 +0530

    english to hindi translator

---
+++ trans.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from googletrans import Translator
+
+win=tk.Tk()
+win.title("Translator")
+win.geometry("200x70")
+
+
+def trans():
+	word=entry.get()
+	translator=Translator(service_urls=['translate.google.com'])
+	translation1=translator.translate(word,dest="hi")
+	label1=tk.Label(win,text=f"Translated in Hindi : {translation1.text}",bg="yellow")
+	label1.grid(row=2,column=0)
+
+label=tk.Label(win,text="Enter Word : ")
+label.grid(row=0,column=0,sticky="W")
+
+entry = tk.Entry(win)
+entry.grid(row=1,column=0)
+
+button = tk.Button(win,text="Translate", command=trans)
+button.grid(row=1,column=2)
+
+win.mainloop()
+
